9ba02d25654612b029310aa02db13ab9f85800d0
denispeyrelon <denis.peyrelon@gmail.com>
2016-10-04 16:53:57 +0200

    First commit

---
+++ app/src/main/java/com/example/denis/myproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.denis.myproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String hello = getResources().getString(R.string.hello_world);
+    }
+}
